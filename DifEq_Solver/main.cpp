a6d1ed1e6bf88622185ff5c37f183db1b90266c0
quasqade <quasqade@gmail.com>
2017-11-14 09:33:40 +0300

    initial

--- DifEq_Solver/main.cpp
+++ DifEq_Solver/main.cpp
@@ -1,7 +1,34 @@
-#include <cstdio>
+﻿#include <cstdio>
+#include <iostream>
+#include <limits>
+
+#define DEBUG_VALUES true
 
 int main()
 {
-    printf("hello from DifEq_Solver!\n");
+	//Постановка задачи
+	std::cout << "Введите порядок ДУ:\n";
+	int order;
+	while (true)
+	{
+		if (DEBUG_VALUES)
+		{
+			order = 1;
+		}
+		else
+		{
+			std::cin >> order;
+		}
+		
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //skip bad input
+		}
+		else
+		{
+			break;
+		}
+	}
     return 0;
 }
